076ffaaa6b56cf334ccbdd9dd04ffda3e8becf93
Theon <theon6208@gmail.com>
2025-03-26 21:55:38 +1100

    feature: build sign in form

--- app/user/signIn/page.tsx
+++ app/user/signIn/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import SignInPage from '@/components/Pages/SignInPage';
+
 export default function signInPage() {
     return (
         <div>
         <h1>Sign In Page</h1>
+        <SignInPage />
         </div>
     )
 }
